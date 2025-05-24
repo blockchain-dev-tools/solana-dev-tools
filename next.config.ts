25ec7c28a1114676c1a4caee2fc5c66384e578ac
devTools <tsingkakuqi@gmail.com>
2025-05-24 16:48:08 +0800

    chore: github page

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.PAGES_BASE_PATH,
+  },
 };
 
 export default nextConfig;
