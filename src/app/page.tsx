c9785fee1b328db1836bcf13d21999ea4889955a
devTools <tsingkakuqi@gmail.com>
2025-05-24 17:01:50 +0800

    chore: config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-
+import { basePath } from '@/config'
 
 export default function Home() {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?  `/${process.env.NEXT_PUBLIC_BASE_PATH}` : "";
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -101,3 +100,4 @@ export default function Home() {
     </div>
   );
 }
+
